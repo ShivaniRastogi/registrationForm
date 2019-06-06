7333d6629eebfee85e0cfb08a2fbd32ac4080dd5
adityasharma263 <aditya.sharma263@gmail.com>
2019-06-06 14:46:28 +0530

    latest code

--- form.js
+++ form.js
@@ -1,16 +1,48 @@
-// var formData = JSON.stringify($("#myForm").serializeArray());
-// $.ajax({
-//     type: "POST",
-//     url: "http://127.0.0.1:9000/test",
-//     data: formData,
-//     success: function(){},
-//     dataType: "json",
-//     contentType : "application/json"
-//   });
-function myFunction(){
-   
-    var inpObj = document.getElementById("id1");
-    alert("Hello! I am an", inpObj);
-    console.log(inpObj.value);
 
+// function myFunction(){
+//     var formData = JSON.stringify($("#myForm").serializeArray());
+//     console.log(formData);
+//     $.ajax({
+//         type: "POST",
+//         url: "http://127.0.0.1:9000/test",
+//         data: formData,
+//         success: function(){},
+//         dataType: "json",
+//         contentType : "application/json"
+//       });
+
+// }
+$(document).ready(function(){
+  var contextroot = "/services/"
+  $("#customerForm").submit(function(e){
+      e.preventDefault();
+      var form = $(this);
+      var action = form.attr("action");
+      var data = form.serializeArray();
+
+      $.ajax({
+                  url: contextroot+action,
+                  dataType: 'json',
+                  type: 'POST',
+                  contentType: 'application/json',
+                  data: JSON.stringify(getFormData(data)),
+                  success: function(data){
+                      console.log("DATA POSTED SUCCESSFULLY"+data);
+                  },
+                  error: function( jqXhr, textStatus, errorThrown ){
+                      console.log( errorThrown );
+                  }
+      });
+});
+});
+
+//utility function
+function getFormData(data) {
+ var unindexed_array = data;
+ var indexed_array = {};
+ $.map(unindexed_array, function(n, i) {
+  indexed_array[n['name']] = n['value'];
+ });
+
+ return indexed_array;
 }
